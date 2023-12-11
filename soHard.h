9cbd50ac2c14bc503aae01344eff01c0eb3f57c4
GhostKaliber <111335322+GhostKaliber@users.noreply.github.com>
2023-12-10 20:06:44 -0800

    finished sophisticated bot

--- soHard.h
+++ soHard.h
@@ -5,42 +5,57 @@
 #include "easyBot.h"
 
 #include <iostream>
-#include <algorithm> //so we can use std max n min
+#include <algorithm> //so we can use max and min, compile w "make force"
+                     //std::max n min work without this library, doesnt 
+                     //seem to impact logic but ill leave it in
 
-int min = -1000;
-int max = 1000;
+int min_int = -999999999;
+int max_int = 999999999;
 
-int minimax(GameState& g, int depth, bool turn, int alpha, int beta){
-    if(turn){
-        int bestScore = min;
-        for (int i = 0; i < g.size; i++) {
-            for (int j = 0; j < g.size; j++) {
-                if (g.grid[i][j] == -1) {
-                    g.grid[i][j] = 1; 
-                    int score = minimax(g, depth + 1, false, alpha, beta);
-                    g.grid[i][j] = -1; 
-                    bestScore = std::max(bestScore, score);
-                    alpha = std::max(alpha, bestScore);
-                    if (beta <= alpha) {
-                        break;
-                    }
-                }
-            }
-        }
-        return bestScore;
+int eval(GameState game){ //dont actually need this ?
+    if(game.hasWon(0)){ // x player, maximizing
+        return (game.size-game.turnCount) + 1;
+    } else if(game.hasWon(1)) { //o player, minimizing
+        return ((game.size-game.turnCount) + 1) * -1;
     } else {
-        int bestScore = max;
-        for (int i = 0; i < g.size; i++) {
-            for (int j = 0; j < g.size; j++) {
-                if (g.grid[i][j] == -1) {
-                    g.grid[i][j] = 0; 
-                    int score = minimax(g, depth + 1, true, alpha, beta);
-                    g.grid[i][j] = -1; 
-                    bestScore = std::min(bestScore, score);
-                    beta = std::min(beta, bestScore);
-                    if (beta <= alpha) {
-                        break;
-                    }
+        return 0; //tie condition score
+    }
+}
+//0 = false, 1 = true
+//minimax algorithm that simulates moves n assigns score
+int minimax(GameState& state, int depth, bool turn)  //depth is look ahead
+{
+    //if statement could be condensed with a ternary thing for min n max
+    int bestScore;
+
+    if(depth >= state.size * state.size || state.done){
+        return eval(state);
+    }
+
+    if(turn){ //maximizing, X = 0
+        bestScore = min_int; //super negative number
+        for(int i = 0;i < state.size; i++){
+            for (int j = 0; j< state.size; j++){
+                if(state.grid[i][j] == -1){
+                    //state.grid[i][j] = 0; //hardcoded playing X/0 instead of using currturn
+                    GameState cp = state;
+                    cp.play(i,j);
+                    bestScore = std::max(bestScore, minimax(cp, depth+1, !turn)); //calc score, turn = false
+                    //state.grid[i][j] = -1; //reverses move
+                }
+            }
+        }
+        return bestScore;
+    } else { //minimizing, O = 1
+        bestScore = max_int; 
+        for (int i = 0; i < state.size; i++){
+            for(int j =0; j < state.size; j++){
+                if(state.grid[i][j]==-1){
+                    //state.grid[i][j] = 1; //hardcoded playing O/1 instead of using currentTurn
+                    GameState cp = state;
+                    cp.play(i,j);
+                    bestScore = std::min(bestScore, minimax(cp, depth+1, !turn));//turn = true
+                    //state.grid[i][j] = -1;
                 }
             }
         }
@@ -48,27 +63,34 @@ int minimax(GameState& g, int depth, bool turn, int alpha, int beta){
     }
 }
 
-Vec findBestMove(GameState game){
-    int bestScore = min;
-    Vec bestMove;
 
-    for (int i = 0; i < game.size; i++) {
-        for (int j = 0; j < game.size; j++) {
-            if(game.grid[i][j] == -1) {
-                game.grid[i][j] = 1;
-                int score = minimax(game, 0, false, min, max);
-                game.grid[i][j] = -1;
-                if(score > bestScore){
-                    bestScore = score;
-                    bestMove.set(i,j);
+
+//vec findbestmove - makes use of minimax to find best score
+Vec findBestMove(GameState game)
+{
+    int bestScore = min_int;
+    Vec bmove(0,0);
+    //bmove.set(-1,-1); //set move to none for now
+
+    for (int i = 0; i < game.size; i++){ //goes thru grid calculating best score w algorithm 
+                                         //lowkey could probs condense into one func w minmax
+            for(int j = 0; j < game.size; j++){
+                if(game.grid[i][j] == -1){ //empty space
+                    GameState cp = game;
+                    cp.play(i,j);
+                    int moveScore = minimax(cp, 0, false);
+                    //game.grid[i][j] = -1;
+                    if(moveScore > bestScore){
+                        bestScore = moveScore;
+                        bmove = Vec(i,j);
+                        //std::cout << "--" << bmove << std::endl;
+                    }
                 }
             }
-
-        }
     }
-    return bestMove;
+    //std::cout << bmove << std::endl;
+    return bmove;
 }
-
 //british - the royals r sophisticated; bot playing logic 
 void britishBot(){
     GameState game;
@@ -77,7 +99,7 @@ void britishBot(){
         std::cout << game << std::endl;
         int x,y;
         
-        if(game.currentTurn){
+        if(game.currentTurn){ //currentTurn = 1
             std::cout << "computer move!!(" << (!game.currentTurn ? "X" : "O") << "):" << std::endl;
             Vec move = findBestMove(game);
             x = move.x;
